ddaedae3dc711a1915c592e054a7cf7e392158a1
kmylo6 <kmylo6@gmail.com>
2021-11-09 16:31:27 -0300

    all stuff

---
+++ packages/component-app-e2e/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'component-app-e2e',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$':  'ts-jest' 
+  },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/component-app-e2e'
+};
